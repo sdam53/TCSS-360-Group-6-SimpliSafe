dd099388fe0aad405d455558f3e8c52330c9d502
Shilnara Dam <ShilnaraWDam@gmail.com>
2021-06-04 19:13:59 -0700

    Added button functionality for Home, Away, and Off.

--- src/view/MainScene.java
+++ src/view/MainScene.java
@@ -1,5 +1,8 @@
 package view;
 
+import controller.Controller;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.ToggleButton;
@@ -7,11 +10,23 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import model.Alarm;
+import model.Component;
+import model.Device;
+import model.Sensor;
+
+import java.awt.event.ActionListener;
 
 /**
  * WIP just a few custom buttons
  */
-public class MainScene {
+public class MainScene implements EventHandler<ActionEvent> {
+
+    private ToggleButton homeButton;
+    private ToggleButton awayButton;
+    private ToggleButton offButton;
+
+
 
     public Pane getPane() {
         return this.pane;
@@ -24,17 +39,82 @@ public class MainScene {
         //A few custom ToggleButtons in an HBOX
         ToggleGroup butonGroup = new ToggleGroup();
         ViewUtil.get().addAlwaysOneSelectedSupport(butonGroup);
-        ToggleButton button1 = new ToggleButton("Home");
-        button1.setToggleGroup(butonGroup);
-        ToggleButton button2 = new ToggleButton("Away");
-        button2.setToggleGroup(butonGroup);
-        ToggleButton button3 = new ToggleButton("Off");
-        button3.setToggleGroup(butonGroup);
-        HBox buttons = new HBox(button1, button2, button3);
+        homeButton = new ToggleButton("Home");
+        homeButton.setToggleGroup(butonGroup);
+        awayButton = new ToggleButton("Away");
+        awayButton.setToggleGroup(butonGroup);
+        offButton = new ToggleButton("Off");
+        offButton.setToggleGroup(butonGroup);
+        HBox buttons = new HBox(homeButton, awayButton, offButton);
 
         buttons.setSpacing(10);
         buttons.setAlignment(Pos.CENTER);
         this.pane.setCenter(buttons);
 
+        homeButton.setOnAction(this);
+        awayButton.setOnAction(this);
+        offButton.setOnAction(this);
+
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == homeButton) {
+            homeState();
+        } else if (actionEvent.getSource() == awayButton) {
+            awayState();
+        } else if (actionEvent.getSource() == offButton) {
+            offState();
+        }
+        System.out.println(Controller.getSensors().size());
+        System.out.println(Controller.getAlarms().size());
+        for (Sensor sensor: Controller.getSensors()) {
+            System.out.println(sensor);
+        }
+        System.out.println("new");
+        for (Alarm alarm: Controller.getAlarms()) {
+            System.out.println(alarm);
+        }
+    }
+
+    /**
+     * will activate all devices except Motion Sensors, they will instead be deactivated
+     */
+    private void homeState() {
+        for (Sensor sensor: Controller.getSensors()) {
+            if (sensor.getMyType() == Component.MOTIONSENSOR) {
+                sensor.setStatus(Device.DEVICE_STATUS.DEACTIVATED);
+            } else {
+                sensor.setStatus(Device.DEVICE_STATUS.ACTIVATED);
+            }
+        }
+        for (Alarm alarm: Controller.getAlarms()) {
+            alarm.setStatus(Device.DEVICE_STATUS.ACTIVATED);
+        }
+
+    }
+
+    /**
+     * Will activate all devices
+     */
+    private void awayState() {
+        for (Sensor sensor: Controller.getSensors()) {
+            sensor.setStatus(Device.DEVICE_STATUS.ACTIVATED);
+        }
+        for (Alarm alarm: Controller.getAlarms()) {
+            alarm.setStatus(Device.DEVICE_STATUS.ACTIVATED);
+        }
+    }
+
+    /**
+     * will deactivate all devices
+     */
+    private void offState() {
+        for (Sensor sensor: Controller.getSensors()) {
+            sensor.setStatus(Device.DEVICE_STATUS.DEACTIVATED);
+        }
+        for (Alarm alarm: Controller.getAlarms()) {
+            alarm.setStatus(Device.DEVICE_STATUS.DEACTIVATED);
+        }
     }
 }
